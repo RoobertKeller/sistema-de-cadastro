3e9f6317e00c20846865ffad2f6432a2e1fac882
RoobertKeller <roobertkeller@gmail.com>
2023-07-20 10:32:03 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-console.log("Carregando design 2.0")
-console.log("Teste 30")

9426b1a099936a33518c0b53a00b95adb6f54fd1
RoobertKeller <roobertkeller@gmail.com>
2023-07-22 19:45:31 -0300

    Teste 45

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Carregando design 2.0")
-console.log("Teste 25")
+console.log("Teste 45")
 
 
